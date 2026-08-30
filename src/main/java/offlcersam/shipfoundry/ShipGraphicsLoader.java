e3bab19f52074d3fa413dbec9cdddb1b7197aab6
OfflcerSam <samuelthetank1@gmail.com>
2026-08-30 15:08:49 -0400

    edit

--- src/main/java/offlcersam/shipfoundry/ShipGraphicsLoader.java
+++ src/main/java/offlcersam/shipfoundry/ShipGraphicsLoader.java
@@ -1,11 +1,11 @@
 package offlcersam.shipfoundry;
 
 import mods.ModLogger;
-import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public final class ShipGraphicsLoader {
@@ -35,7 +35,7 @@ public final class ShipGraphicsLoader {
             return;
         }
 
-        Path entityFolder = FabricLoader.getInstance().getGameDir().resolve(TEXTURE_FOLDER_NAME);
+        Path entityFolder = Paths.get("").toAbsolutePath().resolve(TEXTURE_FOLDER_NAME);
         Path destinationTexture = entityFolder.resolve(textureFileName);
 
         try {
